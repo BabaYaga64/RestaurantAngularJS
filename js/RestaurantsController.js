2c23e8ea7074fe88fa4d949914c6d46640742ea3
David Eliason & Bojana Skarich <student@epicodus.com>
2015-04-20 16:28:26 -0700

    Add CSS functionality

--- js/RestaurantsController.js
+++ js/RestaurantsController.js
@@ -2,7 +2,7 @@ restaurantList.controller('RestaurantsCtrl', function RestaurantsCtrl($scope){
     $scope.restaurants = [];
     $scope.addRestaurant = function(){
         $scope.restaurants.push({ name: $scope.restaurantName, cuisine: $scope.restaurantCuisine, location: $scope.restaurantLocation, price: $scope.restaurantPrice });
-        $scope.restaurantName = null;
+        $scope.restaurantName = null;$scope.restaurantCuisine = null; $scope.restaurantLocation= null; $scope.restaurantPrice= null;
     };
 
     $scope.deleteRestaurant = function(restaurant){
