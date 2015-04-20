fc82766dd277cc3092a73a0973d4efa912720ebe
David Eliason & Bojana Skarich <student@epicodus.com>
2015-04-20 14:28:19 -0700

    Add addRestaurant() method and associated directives in index.html.

---
+++ js/RestaurantsController.js
@@ -0,0 +1,7 @@
+restaurantList.controller('RestaurantsCtrl', function RestaurantsCtrl($scope){
+    $scope.restaurants = [];
+    $scope.addRestaurant = function(){
+        $scope.restaurants.push({ name: $scope.restaurantName });
+        $scope.restaurantName = null;
+    };
+});
